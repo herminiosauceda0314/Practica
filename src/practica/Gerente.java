70483d1c6d61b65ad7741ffd5faec23b1f88363f
herminiosauceda0314 <micuentadosparami@gmail.com>
2026-04-28 15:47:00 -0600

    Clase gerente

---
+++ src/practica/Gerente.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica;
+
+/**
+ *
+ * @author hermi
+ */
+public class Gerente extends Empleado implements Evaluador {
+    private String departamento;
+    private int personasACargo;
+    private double presupuesto;
+    private String frecuenciaReuniones;
+    
+    public Gerente(String nombre, int edad, String documentoIdentidad,  String direccion, String numeroContacto, double salario, String idEmpleado, String fechaContratacion, String tipoContrato, String area, String departamento, int personasACargo, double presupuesto, String frecuenciaReuniones) {
+        super(nombre, edad, documentoIdentidad, direccion, numeroContacto, salario, idEmpleado, fechaContratacion, tipoContrato, area);
+        this.departamento = departamento;
+        this.personasACargo = personasACargo;
+        this.presupuesto = presupuesto;
+        this.frecuenciaReuniones = frecuenciaReuniones;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public int getPersonasACargo() {
+        return personasACargo;
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public String getFrecuenciaReuniones() {
+        return frecuenciaReuniones;
+    }
+    
+    @Override
+    public void trabajar(){
+        System.out.println(nombre+" esta coordinando el equipo del departamento de "+departamento+" ("+personasACargo+" personas a cargo).");
+    }
+
+    @Override
+    public void evaluarDesempeno(String nombreEmpleado) {
+        System.out.println(nombre+"-Gerente- esta evaluando el desempeño de "+nombreEmpleado+" en el departamento de "+departamento+".");
+    }
+    
+    @Override
+    public String toString(){
+        return " -GERENTE- "+super.toString()+
+                "Departamento: "+departamento+
+                "A cargo: "+personasACargo+
+                "Presupuesto: $"+presupuesto+
+                "Reuniones: "+frecuenciaReuniones;
+    }
+    
+}
