6b977e17e81809d5bdc3585668d38f07cd8e0ed4
herminiosauceda0314 <micuentadosparami@gmail.com>
2026-04-28 15:28:13 -0600

    Clase empleado

---
+++ src/practica/Empleado.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica;
+
+/**
+ *
+ * @author hermi
+ */
+public class Empleado extends Persona {
+    protected double salario;
+    protected String idEmpleado;
+    protected String fechaContratacion;
+    protected String tipoContrato;
+    protected String area;
+
+    public Empleado(String nombre, int edad, String dni, String direccion, String numero) {
+        super(nombre, edad, dni, direccion, numero);
+        this.salario=salario;
+        this.idEmpleado=idEmpleado;
+        this.fechaContratacion=fechaContratacion;
+        this.tipoContrato=tipoContrato;
+        this.area=area;
+    }
+    
+    public double getSalario(){
+        return salario;
+    }
+
+    public String getIdEmpleado(){
+        return idEmpleado;
+    }
+
+    public String getFechaContratacion(){
+        return fechaContratacion;
+    }
+
+    public String getTipoContrato(){
+        return tipoContrato;
+    }
+
+    public String getArea(){
+        return area;
+    }
+    
+    public void trabajar(){
+        System.out.println(nombre+" esta realizando labores generales.");
+    }
+    
+}
